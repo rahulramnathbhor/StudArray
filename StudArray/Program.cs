03f54f9a79c30ad13c70ab69d983c4fdda6ed9a0
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-19 17:48:45 +0530

    Add project files.

---
+++ StudArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudArray
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string[] names = new string[4];
+
+            names[0] = "chiu";
+            names[1] = "rahul";
+            names[2] = "shubham";
+            names[3] = "mahesh";
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                string s = names[i];
+                Console.WriteLine(s);
+                
+                
+
+            }
+        }
+            
+
+
+        
+    }
+}
